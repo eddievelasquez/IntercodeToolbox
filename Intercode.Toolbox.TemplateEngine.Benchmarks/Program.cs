ffbe9b4a0237e53256450be69ee332ae98c3e413
Eddie Velasquez <guardi@e-velasquez.com>
2025-09-13 08:17:50 -0700

    TemplateEngine benchmarks are now compatible with v3
    - Updated README to include benchmark results.

--- Intercode.Toolbox.TemplateEngine.Benchmarks/Program.cs
+++ Intercode.Toolbox.TemplateEngine.Benchmarks/Program.cs
@@ -1,40 +1,8 @@
-// Module Name: Program.cs
-// Author:      Eduardo Velasquez
-// Copyright (c) 2024, Intercode Consulting, Inc.
+// // Module Name: Program.cs
+// // Author:      Eddie Velasquez
+// // Copyright (c) 2025, Intercode Consulting, Inc.
 
-using System.Globalization;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using Intercode.Toolbox.TemplateEngine.Benchmarks;
-using Perfolizer.Horology;
-using Perfolizer.Metrology;
 
-#if PROFILING
-var tests = new MacroProcessingBenchmarks();
-
-for( int i = 0; i < 100; ++i )
-{
-  tests.UsingTemplateEngine();
-}
-
-return;
-#else
-
-var exporter = new CsvExporter(
-  CsvSeparator.CurrentCulture,
-  new SummaryStyle(
-    cultureInfo: CultureInfo.CurrentCulture,
-    printUnitsInHeader: true,
-    printUnitsInContent: false,
-    timeUnit: TimeUnit.Nanosecond,
-    sizeUnit: SizeUnit.B
-  ));
-
-var config = ManualConfig.CreateMinimumViable().AddExporter(exporter);
-
-BenchmarkSwitcher.FromAssembly( typeof( MacroProcessingBenchmarks ).Assembly )
-                 .Run( args );
-
-#endif
+BenchmarkSwitcher.FromAssembly(typeof(MacroProcessingBenchmarks).Assembly).Run(args);
